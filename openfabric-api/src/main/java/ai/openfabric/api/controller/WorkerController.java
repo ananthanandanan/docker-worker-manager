cd7721c580ef28ea3cf5750175ad9865cda73602
ananthanandanan <ananthanandanan@gmail.com>
2023-05-12 17:31:38 +0530

    chore: Integrated docker-java client - Add dependencies - Create bean to handle Docker java client - create a test API for listing containers

--- openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
+++ openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
@@ -1,16 +1,33 @@
 package ai.openfabric.api.controller;
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("${node.api.path}/worker")
 public class WorkerController {
 
+    private final DockerClient dockerClient;
+    public WorkerController(DockerClient dockerClient) {
+        this.dockerClient = dockerClient;
+    }
 
     @PostMapping(path = "/hello")
     public @ResponseBody String hello(@RequestBody String name) {
         return "Hello!" + name;
     }
 
+    @GetMapping(path = "/containers")
+    public @ResponseBody List<Container> containersAvailable() {
+        List<Container> containers = dockerClient.listContainersCmd().exec();
 
+        if(containers!=null){
+            return containers;
+        }
+
+        return null;
+    }
 }
