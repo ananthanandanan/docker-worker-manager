20fc4b33a1cf597238e1820aaa7dc0a048761a04
ananthanandanan <ananthanandanan@gmail.com>
2023-05-15 00:25:50 +0530

    chore: add new custom getter for pageable

--- openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
+++ openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
@@ -1,9 +1,12 @@
 package ai.openfabric.api.repository;
 
 import ai.openfabric.api.model.Worker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WorkerRepository extends CrudRepository<Worker, String> {
     Worker findByContainerId(String containerId);
+    Page<Worker> findAll(Pageable pageable);
 
 }
